fea3862a3237a5754c98593aed6f461add0c8bcc
Maria Cruz Schena <mariacruzschena@gmail.com>
2023-03-27 16:30:53 -0300

    added crud user by admin

---
+++ routes/admin.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const AdminController = require("../controllers/admin");
+const { validateUser } = require("../middleware/auth");
+
+router.get("/users", AdminController.allUsers);
+router.post("/create/user", AdminController.createUser);
+//router.put("/edit/user", AdminController.editUser);
+router.delete("/users", AdminController.deleteUser);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const userRouter = require("./user");
 const officesRouter = require("./offices");
+const adminRouter = require("./admin");
 
 router.use("/user", userRouter);
-
 router.use("/offices", officesRouter);
+router.use("/admin", adminRouter);
 
 
 
