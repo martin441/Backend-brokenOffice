eb17b53b5b7ac4d79792f287f9c21c4a7af91874
Jesica Yu <jesicayu@umich.edu>
2023-03-27 16:32:30 -0300

    create office routes

--- controllers/offices.js
+++ controllers/offices.js
@@ -12,6 +12,41 @@ class OfficesController {
       res.status(404).send(error);
     }
   }
+  static async addOffice(req, res, next) {
+    try {
+      const {error,data} = await OfficesServices.addNewOffice(req.body);
+      if(error){
+        return res.status(404).send(data)
+      }
+      res.status(201).send(data)
+    } catch (error) {
+      res.status(404).send(error)
+    }
+  }
+  static async deleteOffice(req, res, next) {
+    try {
+      const {officeId} = req.params
+      const {error, data} = await OfficesServices.deleteAnOffice(officeId)
+      if(error){
+        return res.status(404).send(data)
+      }
+      res.status(204).send()
+    } catch (error) {
+      res.status(404).send(error)
+    }
+  }
+  static async editOffice(req, res, next) {
+    try {
+      const {officeId} = req.params
+      const {error,data} = await OfficesServices.editAnOffice(req.body, officeId)
+      if(error){
+        return res.status(404).send(data)
+      }
+      res.status(202).send(data)
+    } catch (error) {
+      res.status(404).send(error)
+    }
+  }
 }
 
 module.exports = OfficesController;
--- routes/offices.js
+++ routes/offices.js
@@ -3,6 +3,9 @@ const OfficesController = require("../controllers/offices");
 const router = express.Router();
 const { validateUser } = require("../middleware/auth");
 
-router.get("/", validateUser, OfficesController.getOffices)
+router.get("/", OfficesController.getOffices);
+router.post("/add", OfficesController.addOffice);
+router.delete("/delete/:officeId", OfficesController.deleteOffice);
+router.put("/edit/:officeId", OfficesController.editOffice);
 
 module.exports = router;
