0d0430dd84f11c91ef329532e6ed25ed77f816f3
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-03-27 19:16:45 -0300

    feat: middlewares type

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ const { validateToken } = require("../utils/tokens");
 function validateUser(req, res, next) {
     const token = req.cookies.token;
     if (!token) return res.status(401).send("Invalid credentials");
-  
     const {payload}  = validateToken(token);
     if (!payload) return res.status(401).send("Invalid credentials");
     req.user = payload;
