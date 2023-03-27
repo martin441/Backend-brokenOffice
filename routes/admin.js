68cb78aa894c21c525437c14622ead86c00c024f
Maria Cruz Schena <mariacruzschena@gmail.com>
2023-03-27 17:27:08 -0300

    added edit user privileges as admin

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ const { validateUser } = require("../middleware/auth");
 
 router.get("/users", AdminController.allUsers);
 router.post("/create/user", AdminController.createUser);
-//router.put("/edit/user", AdminController.editUser);
+router.put("/edit/user", AdminController.editUser);
 router.delete("/users", AdminController.deleteUser);
 
 module.exports = router;
