7bb837a186286765982a129974b84a5c8d6ecaea
Jesica Yu <jesicayu@umich.edu>
2023-03-30 12:40:17 -0300

    middlewares arreglados

--- middleware/validateOmega.js
+++ middleware/validateOmega.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const { OMEGA } = process.env;
 
-function validateOmega (req, res, next) {
+function validateOmega(req, res, next) {
   const user = req.user;
-  if (!user.type === OMEGA) return res.status(401).send("Invalid credentials");
+  if (user.type !== OMEGA) return res.status(401).send("Invalid credentials");
   next();
 }
 
