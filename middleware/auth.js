6de1da102ef8c85848c63e0bf18633b39fb1d744
Martin Machado <martinmachado441@gmail.com>
2023-05-31 10:47:03 -0300

    console log back end token

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,7 @@ const { validateToken } = require("../utils/tokens");
 
 function validateUser(req, res, next) {
   const token = req.cookies.token; //la forma de express para obtener el header
+  console.log("token del logout del back", token);
   if (!token) return res.status(401).send("Invalid credentials");
   const { payload } = validateToken(token);
   if (!payload) return res.status(401).send("Invalid credentials");
